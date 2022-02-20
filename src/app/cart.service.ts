2c1ae412c71916ff436857b649bccd168030624d
Miljoen <yoerimoens@gmail.com>
2022-02-20 22:43:52 +0100

    Add shipping prices

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,13 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Product } from './products';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
-  items: Product[] = []
+  items: Product[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   addToCart(product: Product) {
     this.items.push(product);
@@ -20,5 +21,9 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json')
   }
 }
